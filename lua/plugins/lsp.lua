cada5e919b73c3a4f667c191ebb434a59262acb9
sahari <mjsahari@gmail.com>
2024-06-30 18:39:37 -0700

    feat: update go settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -67,7 +67,17 @@ return {
 
       -- Language Specific
       { "folke/neodev.nvim" },                  -- lua
-      { 'crispgm/nvim-go' },                    -- golang
+      {
+        "ray-x/go.nvim",                        -- Go
+        dependencies = {
+          "ray-x/guihua.lua",
+          "neovim/nvim-lspconfig",
+          "nvim-treesitter/nvim-treesitter",
+        },
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+      },
       { 'hashivim/vim-terraform' },             -- terraform
       { 'simrat39/rust-tools.nvim' },           -- rust
       { "jose-elias-alvarez/typescript.nvim" }, -- typescript
@@ -329,39 +339,10 @@ return {
       })
 
       require("go").setup({
-        -- notify: use nvim-notify
-        notify = true,
-        -- auto commands
-        auto_format = true,
-        auto_lint = true,
-        -- linters: revive, errcheck, staticcheck, golangci-lint
-        linter = 'golangci-lint',
-        -- linter_flags: e.g., {revive = {'-config', '/path/to/config.yml'}}
-        linter_flags = {},
-        -- lint_prompt_style: qf (quickfix), vt (virtual text)
-        lint_prompt_style = 'qf',
-        -- formatter: goimports, gofmt, gofumpt
-        formatter = 'gofumpt',
-        -- maintain cursor position after formatting loaded buffer
-        maintain_cursor_pos = false,
-        -- test flags: -count=1 will disable cache
-        test_flags = { '-v' },
-        test_timeout = '30s',
-        test_env = {},
-        -- show test result with popup window
-        test_popup = true,
-        test_popup_auto_leave = false,
-        test_popup_width = 80,
-        test_popup_height = 10,
-        -- test open
-        test_open_cmd = 'edit',
-        -- struct tags
-        tags_name = 'json',
-        tags_options = { 'json=omitempty' },
-        tags_transform = 'snakecase',
-        tags_flags = { '-skip-unexported' },
-        -- quick type
-        quick_type_flags = { '--just-types' },
+        lsp_cfg = {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        }
       })
 
       require('rust-tools').setup()
